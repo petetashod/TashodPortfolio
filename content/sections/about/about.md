1689ce43e7145299abf80ae7cd975043045bed8e
tashod peterson <tashodpeterson@tashods-Air.lan>
2023-08-28 21:12:34 -0400

    first

---
+++ content/sections/about/about.md
@@ -0,0 +1,9 @@
+---
+imageSrc: "../../images/charles-deluvio-DgoyKNgPiFQ-unsplash.jpg"
+imageAlt: ""
+---
+Learning how to code was always a passion of mine. It started with writing command line code at a career center in Highschool to writing Html and Css in College. I thrive on transforming complex challenges into elegant, functional solutions through the art of programming. For me, writing code isn't just a skill; it's a way to bring creativity to life in the digital realm and contribute meaningfully to the world of technology.
+
+
+
+Photo by <a href="https://unsplash.com/@charlesdeluvio?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="nofollow noopener noreferrer" aria-label="External Link"><u>Charles Deluvio</u></a> on Unsplash
---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Tashod Peterson's Portfolio" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <AboutSection sectionId="about" heading="About" />
+        <InterestsSection sectionId="details" heading="Interest" />
+        <ProjectsSection sectionId="features" heading="Projects" />
+        <ContactSection sectionId="github" heading="" />
+      </Page>
+    </>
+  );
+}
